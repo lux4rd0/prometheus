532d646004621566b50dc8970e290ad5f8f98d09
Dave Schmid <dave@pulpfree.org>
2020-11-07 17:29:54 -0600

    Remove timer (for now)

--- darkstat-exporter/scripts/darkstat.sh
+++ darkstat-exporter/scripts/darkstat.sh
@@ -6,12 +6,6 @@
 # https://github.com/lux4rd0/prometheus/tree/main/darkstat-exporter
 #
 
-# Timer Start
-
-start_at=$(date +%s,%N)
-_s1=$(echo "$start_at" | cut -d',' -f1)   # sec
-_s2=$(echo "$start_at" | cut -d',' -f2)   # nano sec
-
 # Change value to represent URL of your DarkStat instance
 
 tmp_page=$(curl -s "http://gw.tylephony.com:666/hosts/?full=yes")
@@ -43,14 +37,3 @@ tx=$(echo "$tmp_page" |grep -A 3 -i "${line}" |tail -n 1 |sed 's/ <td class="num
 echo "darkstat_net_out{host=\"${line}\"} ${tx}"
 
 done
-
-# Timer End
-
-end_at=$(date +%s,%N)
-_e1=$(echo "$end_at" | cut -d',' -f1)
-_e2=$(echo "$end_at" | cut -d',' -f2)
-duration=$(bc <<< "scale=3; $_e1 - $_s1 + ($_e2 -$_s2)/1000000000")
-
-echo "# HELP darkstat_timer_seconds Darkstat script timer metric"
-echo "# TYPE darkstat_timer_seconds gauge"
-echo "darkstat_timer_seconds ${duration}"
